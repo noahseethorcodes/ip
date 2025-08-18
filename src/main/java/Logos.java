c003163040a00fb75aa948bc9a96f0428dd2dec0
Noah Seethor <noahseethor@gmail.com>
2025-08-18 18:28:20 +0800

    Implemented Level-0: Rename, Greet, Exit

---
+++ src/main/java/Logos.java
@@ -0,0 +1,30 @@
+public class Logos {
+
+    private static String Line = "--------------------------------------";
+    private static String ChatbotName = "Logos";
+    public static void main(String[] args) {
+        String logo = " _                           \n"       
+                    + "| |    ___   __ _  ___  ___  \n"
+                    + "| |   / _ \\ / _` |/ _ \\/ __| \n"
+                    + "| |__| (_) | (_| | (_) \\__ \\ \n"
+                    + "|_____\\___/ \\__, |\\___/|___/ \n"
+                    + "            |___/            \n";
+        System.out.println("Welcome to...\n" + logo);
+        System.out.println(Line);
+        System.out.println("Hello! I'm " + Logos.ChatbotName);
+        System.out.println("What can I do for you?");
+        System.out.println(Line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(Line);
+    }
+}
+
+
+/*
+" _                          \n" +        
+"| |    ___   __ _  ___  ___ \n" +
+"| |   / _ \ / _` |/ _ \/ __|\n" +
+"| |__| (_) | (_| | (_) \__ \\n" +
+"|_____\___/ \__, |\___/|___/\n" +
+"            |___/           \n"
+ */
